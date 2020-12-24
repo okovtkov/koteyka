ba7a2f0ebcdf955ed0a094620123076dfe31cbb0
Oleg <oleg_kovtkov@mail.ru>
2020-12-25 02:20:27 +0300

    сделал комнаты с индивидуалити свойствами, поисправлял некоторые баги на главной

---
+++ source/data/rooms.js
@@ -0,0 +1,57 @@
+let economRoom = {
+    image: "/img/econom-room.jpg",
+    name: "Эконом",
+    size: "90х70х180 см",
+    area: "0,63 м2",
+    comforts: [],
+    price: 100
+};
+
+let economPlusRoom = {
+    image: "/img/econom+.jpg",
+    name: "Эконом-плюс",
+    size: "90х100х180 см",
+    area: "0,90 м2",
+    comforts: ["лежак", "когтеточка"],
+    price: 200
+};
+
+let comfortRoom = {
+    image: "/img/comfort.jpg",
+    name: "Комфорт",
+    size: "100х125х180 см",
+    area: "1,13 м2",
+    comforts: ["лежак", "когтеточка", "клубок"],
+    price: 250
+};
+
+let suitRoom = {
+    image: "/img/suit.jpg",
+    name: "Сьют",
+    size: "125х125х180 см",
+    area: "1,56 м2",
+    comforts: ["лежак", "когтеточка", "клубок"],
+    price: 350
+};
+
+let luxRoom = {
+    image: "/img/lux.jpg",
+    name: "Люкс",
+    size: "160х160х180 см",
+    area: " 2,56 м2",
+    comforts: ["лежак", "когтеточка", "клубок", "домик"],
+    price: 500
+};
+
+let presidentRoom = {
+    image: "/img/president.jpg",
+    name: "Президентский",
+    size: "180х160х180 см",
+    area: "2,88 м2",
+    comforts: ["лежак", "когтеточка", "клубок", "домик"],
+    price: 600
+};
+
+let rooms = [economRoom, economPlusRoom, comfortRoom, suitRoom, luxRoom, presidentRoom];
+
+module.exports = rooms;
