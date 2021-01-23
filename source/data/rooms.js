71033f6c730d28e5694c5ab7eb1701c4ad74ba63
Oleg <oleg_kovtkov@mail.ru>
2021-01-23 18:35:43 +0300

    создал странички для каждой комнатки

--- source/data/rooms.js
+++ source/data/rooms.js
@@ -4,6 +4,7 @@ let economRoom = {
     size: "90х70х180 см",
     area: "0,63 м2",
     comforts: [],
+    link: "econom",
     price: 100
 };
 
@@ -13,6 +14,7 @@ let economPlusRoom = {
     size: "90х100х180 см",
     area: "0,90 м2",
     comforts: ["лежак", "когтеточка"],
+    link: "econom-plus",
     price: 200
 };
 
@@ -22,6 +24,7 @@ let comfortRoom = {
     size: "100х125х180 см",
     area: "1,13 м2",
     comforts: ["лежак", "когтеточка", "клубок"],
+    link: "comfort",
     price: 250
 };
 
@@ -31,8 +34,9 @@ let suitRoom = {
     size: "125х125х180 см",
     area: "1,56 м2",
     comforts: ["лежак", "когтеточка", "клубок"],
+    link: "suit",
     price: 350
-};
+}; 
 
 let luxRoom = {
     image: "/img/room-2.jpg",
@@ -40,6 +44,7 @@ let luxRoom = {
     size: "160х160х180 см",
     area: " 2,56 м2",
     comforts: ["лежак", "когтеточка", "клубок", "домик"],
+    link: "lux",
     price: 500
 };
 
@@ -49,6 +54,7 @@ let presidentRoom = {
     size: "180х160х180 см",
     area: "2,88 м2",
     comforts: ["лежак", "когтеточка", "клубок", "домик"],
+    link: "president",
     price: 600
 };
 
